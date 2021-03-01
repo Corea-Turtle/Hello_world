319d5a689f4d84ab262e83f0b281f9c419f1a72e
Corea-Turtle <78157221+Corea-Turtle@users.noreply.github.com>
2021-03-01 20:22:22 +0900

    Create 4-12.sum_array.c

---
+++ 4-12.sum_array.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+main()
+{
+	int mat[3][4] = {
+		{20, 42, 70, 34},
+		{67, 98, 37, 41},
+		{76, 99, 43, 65}
+	};
+	int i, j;
+	int sum_r; //가로줄의 합
+	int sum_c[4] = { 0,0,0,0 }; //세로줄의 합
+	int total = 0;  //모든 수의 합계
+
+	//각요소의 표시와 계산
+	for (j = 0; j < 3; j++) {
+		sum_r = 0;
+		for (i = 0; i < 4; i++) {
+			printf("%4d ", mat[j][i]);
+			sum_r += mat[j][i];
+			sum_c[i] += mat[j][i];
+		}
+		printf("| %4d\n", sum_r);
+		}
+
+	printf("--------------------+-----\n");
+	for (i = 0; i < 4; i++) {
+		printf("%4d ", sum_c[i]);
+		total += sum_c[i];
+	}
+	printf("| %4d\n", total);
+}
