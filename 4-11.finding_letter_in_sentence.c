e4f56367afd037edbb241969be6bf22daa32eaf2
Corea-Turtle <78157221+Corea-Turtle@users.noreply.github.com>
2021-03-01 20:21:22 +0900

    Update 4-11.finding_letter_in_sentence.c

--- 4-11.finding_letter_in_sentence.c
+++ 4-11.finding_letter_in_sentence.c
@@ -23,4 +23,17 @@ main()
 
 /*
  10번쨰 줄 이스케이프 시퀸스 참조 https://ko.wikipedia.org/wiki/%EC%9D%B4%EC%8A%A4%EC%BC%80%EC%9D%B4%ED%94%84_%EC%8B%9C%ED%80%80%EC%8A%A4#:~:text=%EC%9D%B4%EC%8A%A4%EC%BC%80%EC%9D%B4%ED%94%84%20%EC%8B%9C%ED%80%80%EC%8A%A4(escape%20sequence)%20%EB%98%90%EB%8A%94,%EC%A7%80%EB%8B%88%EA%B3%A0%20%EC%9E%88%EB%8A%94%20%ED%8A%B9%EC%88%98%20%EB%AC%B8%EC%9E%90%EC%9D%B4%EB%8B%A4.
+
+
+while 반복문을 이렇게 대체할 수 있다.
+이떄 strchr()함수를 쓰기 위해 헤더에 #include<string.h>를 추가해야한다.
+while(1){
+    p = strchr(p, c)
+    if(!p)
+       break;
+    printf("%d번쨰에서 발견되었습니다.\n\n",p-s+1);
+    n++;
+    p++; //찾아낸 문자의 다음위치 부터 검색(s의 어드레스 +1)
+}
+
 */
