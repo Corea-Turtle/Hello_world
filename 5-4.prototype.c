bc5128b2401969f1277c3f7ea1261f0fe0650a8c
Corea-Turtle <78157221+Corea-Turtle@users.noreply.github.com>
2021-03-04 11:10:08 +0900

    Create 5-4.prototype.c

---
+++ 5-4.prototype.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void diospnum(int a);
+
+main()
+{
+      int x = 10;
+  dispnum(5);
+  dispnum(x);
+}
+
+void dispnum(int a)
+{
+    printf("인수의 값은 %d\n", a);
+}
+
+/*
+프로토 타입 선언과 호출 부분의
+반환값의 형, 인수의 개수, 형, 나열 순서를 같게 해줘야한다.
+*/
